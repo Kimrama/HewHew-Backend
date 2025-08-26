5d1d6fde87077315f0c3d694cc1093ed3f8ad439
suwichk007 <suwichakboatkung@gmail.com>
2025-08-26 13:39:17 +0700

    Test by Boat

--- server/miscellaneousRouter.go
+++ server/miscellaneousRouter.go
@@ -2,6 +2,16 @@ package server
 
 import "github.com/gofiber/fiber/v2"
 
+type User struct {
+	ID   string `json:"id" validate:"required"`
+	Name string `json:"name" validate:"required"`
+}
+
+var userList = []User{
+	{ID: "1", Name: "kimrama"},
+	{ID: "2", Name: "Boat"},
+}
+
 func (s *fiberServer) initMiscellaneousRoutes() {
 	router := s.app.Group("/v1/misc")
 
@@ -10,4 +20,38 @@ func (s *fiberServer) initMiscellaneousRoutes() {
 			"status": "healthy---v1----test CI/CD",
 		})
 	})
+
+	router.Get("/users", func(c *fiber.Ctx) error {
+		return c.JSON(userList)
+	})
+
+	router.Get("/kimrama", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"name": "kimrama",
+		})
+	})
+
+	router.Post("/users", func(c *fiber.Ctx) error {
+		var newUser User
+		if err := c.BodyParser(&newUser); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+		}
+		userList = append(userList, newUser)
+		return c.Status(fiber.StatusCreated).JSON(newUser)
+	})
+
+	router.Get("/kimrama/:number", func(c *fiber.Ctx) error {
+		number := c.Params("number")
+		return c.JSON(fiber.Map{
+			"name":   "kimrama",
+			"number": number,
+		})
+	})
+
+	router.Get("/Boat", func(c *fiber.Ctx) error {
+		return c.JSON((fiber.Map{
+			"name": "Boat",
+		}))
+	})
+
 }
