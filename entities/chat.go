14e15b9fd90fb13763b6dc1847075089c9b286fd
Kimrama <kimrama0x@gmail.com>
2025-08-27 12:47:41 +0700

    add time stmap and map to order

--- entities/chat.go
+++ entities/chat.go
@@ -1,11 +1,15 @@
 package entities
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 )
 
 type Chat struct {
-	ChatID  uuid.UUID `gorm:"primaryKey"`
-	Sender  uuid.UUID `gorm:"not null"`
-	Message string    `gorm:"not null"`
+	ChatID    uuid.UUID `gorm:"primaryKey"`
+	Sender    uuid.UUID `gorm:"not null"`
+	Message   string    `gorm:"not null"`
+	OrderID   uuid.UUID `gorm:"not null"`
+	TimeStamp time.Time `gorm:"autoCreateTime:milli"`
 }
