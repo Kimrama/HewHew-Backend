0319f0fd77c93b93eca51c2f7dd6dac518970d7a
Kimrama <kimrama0x@gmail.com>
2025-08-27 12:51:58 +0700

    add UserReviewerID and UserTargetID fields to Review struct

--- entities/review.go
+++ entities/review.go
@@ -1,10 +1,17 @@
 package entities
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type Review struct {
-	ReviewID uuid.UUID `gorm:"primaryKey"`
-	OrderID  uuid.UUID `gorm:"not null"`
-	Rating   int       `gorm:"not null"`
-	Comment  string    `gorm:"size:512"`
+	ReviewID       uuid.UUID `gorm:"primaryKey"`
+	UserReviewerID uuid.UUID `gorm:"not null"`
+	UserTargetID   uuid.UUID `gorm:"not null"`
+	OrderID        uuid.UUID `gorm:"not null"`
+	Rating         int       `gorm:"not null"`
+	Comment        string    `gorm:"size:512"`
+	TimeStamp      time.Time `gorm:"autoCreateTime:milli"`
 }
