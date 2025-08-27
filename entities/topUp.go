693c3ce379e642b748763cddae64f3348533e491
Kimrama <kimrama0x@gmail.com>
2025-08-27 12:52:10 +0700

    add TimeStamp field to TopUp struct for automatic timestamping

--- entities/topUp.go
+++ entities/topUp.go
@@ -1,9 +1,14 @@
 package entities
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type TopUp struct {
-	TopUpID uuid.UUID `gorm:"primaryKey"`
-	UserID  uuid.UUID `gorm:"not null"`
-	Amount  float64   `gorm:"not null"`
+	TopUpID   uuid.UUID `gorm:"primaryKey"`
+	UserID    uuid.UUID `gorm:"not null"`
+	Amount    float64   `gorm:"not null"`
+	TimeStamp time.Time `gorm:"autoCreateTime:milli"`
 }
