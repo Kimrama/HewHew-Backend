e58432a41354735c4a43afa7ad7f18dde0f228c4
Kimrama <kimrama0x@gmail.com>
2025-08-27 12:52:16 +0700

    add TransactionLog struct to manage transaction records

---
+++ entities/transactionLog.go
@@ -0,0 +1,16 @@
+package entities
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type TransactionLog struct {
+	TransactionLogID uuid.UUID `gorm:"primaryKey"`
+	TargetUserID     uuid.UUID `gorm:"not null"`
+	OrderID          uuid.UUID `gorm:"uniqueIndex;not null"`
+	TimeStamp        time.Time `gorm:"autoCreateTime:milli"`
+	Detail           string    `gorm:"size:256;not null"`
+	Amount           float64   `gorm:"not null"`
+}
