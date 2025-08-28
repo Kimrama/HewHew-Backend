7427fbd1a8d19e376b37bec1cab782d915aa00b9
Kimrama <kimrama0x@gmail.com>
2025-08-28 23:26:43 +0700

    feat: add image preprocessing utility for uploading and resizing images

---
+++ utils/preprocessUploadImage.go
@@ -0,0 +1,48 @@
+package utils
+
+import (
+	"bytes"
+	"fmt"
+	"image"
+	"image/jpeg"
+	"image/png"
+	"mime/multipart"
+	"path/filepath"
+	"strings"
+
+	"github.com/nfnt/resize"
+)
+
+func PreprocessUploadImage(file *multipart.FileHeader) ([]byte, error) {
+	src, err := file.Open()
+	if err != nil {
+		return nil, err
+	}
+	defer src.Close()
+
+	// decode image
+	img, _, err := image.Decode(src)
+	if err != nil {
+		return nil, err
+	}
+
+	// resize to 400x400
+	resizedImg := resize.Resize(400, 400, img, resize.Lanczos3)
+
+	// encode by filetype
+	buf := new(bytes.Buffer)
+	ext := strings.ToLower(filepath.Ext(file.Filename))
+
+	switch ext {
+	case ".jpg", ".jpeg":
+		err = jpeg.Encode(buf, resizedImg, nil)
+	case ".png":
+		err = png.Encode(buf, resizedImg)
+	default:
+		err = fmt.Errorf("unsupported file type: %s", ext)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
