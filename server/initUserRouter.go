93428511463b527459ea511188b1747164de87e2
Kimrama <kimrama0x@gmail.com>
2025-08-29 14:30:38 +0700

    refactor: streamline user repository and service initialization in InitUserRouter

--- server/initUserRouter.go
+++ server/initUserRouter.go
@@ -7,12 +7,11 @@ import (
 )
 
 func (s *fiberServer) InitUserRouter() {
-	userRepository := _userRepository.NewUserRepositoryImpl(s.db)
-	userService := _userService.NewUserServiceImpl(userRepository, s.conf.Supabase)
+	userRepository := _userRepository.NewUserRepositoryImpl(s.db, s.conf.Supabase)
+	userService := _userService.NewUserServiceImpl(userRepository)
 	userController := _userController.NewUserControllerImpl(userService)
 
 	userGroup := s.app.Group("/v1/user")
 	userGroup.Get("/", userController.GetUsers)
 	userGroup.Post("/register", userController.CreateUser)
-	userGroup.Post("/test", userController.TestUser)
 }
