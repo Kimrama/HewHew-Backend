1bbcdcd22019a698bd6e209ca7fc13abd099a12b
Thirdsupakarn <137022989+Thirdsupakarn@users.noreply.github.com>
2025-09-03 11:08:19 +0700

    refactor: update route paths for EditUserProfileImage and EditUser methods to use a consistent parameter format

--- server/initUserRouter.go
+++ server/initUserRouter.go
@@ -15,6 +15,6 @@ func (s *fiberServer) initUserRouter() {
 	userGroup.Get("/:username", userController.GetUserByUsername)
 	userGroup.Post("/register", userController.CreateUser)
 	userGroup.Post("/login", userController.LoginUser)
-	userGroup.Put("/edit-profile-image/:id", userController.EditUserProfileImage)
-	userGroup.Put("/edit-user/:id", userController.EditUser)
+	userGroup.Put("/:id/profile-image", userController.EditUserProfileImage)
+	userGroup.Put("/:id", userController.EditUser)
 }
