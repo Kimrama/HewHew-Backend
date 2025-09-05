ba0111a7d83a09e3e8d14c95d8c222a9fbafbeff
Thirdsupakarn <137022989+Thirdsupakarn@users.noreply.github.com>
2025-09-05 09:53:42 +0700

    feat: implement shop module with controller, service, and repository layers

---
+++ pkg/shop/controller/shopControllerImpl.go
@@ -0,0 +1,21 @@
+package controller
+
+import (
+	"hewhew-backend/pkg/shop/service"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type ShopControllerImpl struct {
+    ShopService service.ShopService
+}
+
+func NewShopControllerImpl(ShopService service.ShopService) ShopController {
+    return &ShopControllerImpl{
+        ShopService: ShopService,
+    }
+}
+
+func (s *ShopControllerImpl) createShop(ctx *fiber.Ctx) error {
+    return nil
+}
