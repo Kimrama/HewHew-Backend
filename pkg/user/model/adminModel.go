fd9f1464cb28eafe83b644572b57315ff3bae59e
suwichk007 <suwichakboatkung@gmail.com>
2025-09-11 22:22:44 +0700

    feat: update CreateAdminRequest struct with JSON tags and ensure State field in Shop struct is defined

--- pkg/user/model/adminModel.go
+++ pkg/user/model/adminModel.go
@@ -1,11 +1,11 @@
 package model
 
 type CreateAdminRequest struct {
-	Username string
-	Password string
-	FName    string
-	LName    string
-	name     string  
-	address  string   
-	canteen_name   string    
+	Username string `json:"username"`
+	Password string `json:"password"`
+	FName    string `json:"fname"`
+	LName    string `json:"lname"`
+	Name     string `json:"name"`
+	Address  string `json:"address"`
+	CanteenName string `json:"canteen_name"`
 }
