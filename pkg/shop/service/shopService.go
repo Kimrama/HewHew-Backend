27df6c2446c89ccaff7b0ae253562225140ec59c
suwichk007 <suwichakboatkung@gmail.com>
2025-09-12 21:36:16 +0700

    feat: implement GetShopByAdminID, ChangeState, EditShopImage, and EditShop methods in ShopServiceImpl

--- pkg/shop/service/shopService.go
+++ pkg/shop/service/shopService.go
@@ -1,9 +1,20 @@
 package service
 
-import "hewhew-backend/entities"
+import (
+	"hewhew-backend/entities"
+	"hewhew-backend/pkg/shop/model"
+	"hewhew-backend/utils"
+
+	"github.com/google/uuid"
+)
 
 type ShopService interface {
 	CreateCanteen(canteenModel interface{}) error
 	EditCanteen(canteenName string,canteenEntity *entities.Canteen) error
 	DeleteCanteen(canteenID string) error
+	GetShopByAdminID(adminID uuid.UUID) (*entities.Shop, error)
+	EditShop(body model.EditShopRequest, shop uuid.UUID) error
+	ChangeState(body model.ChangeState, admin_id uuid.UUID) error
+	EditShopImage(shopID uuid.UUID, imageModel *utils.ImageModel) error
+	GetShopAdminByUsername(username string) (*entities.ShopAdmin, error)
 }
--- pkg/shop/service/shopServiceImpl.go
+++ pkg/shop/service/shopServiceImpl.go
@@ -1,9 +1,14 @@
 package service
 
 import (
+	"errors"
 	"fmt"
 	"hewhew-backend/entities"
+	"hewhew-backend/pkg/shop/model"
 	"hewhew-backend/pkg/shop/repository"
+	"hewhew-backend/utils"
+
+	"github.com/google/uuid"
 )
 
 type ShopServiceImpl struct {
@@ -28,4 +33,51 @@ func (s *ShopServiceImpl) EditCanteen(canteenName string,canteenEntity *entities
 
 func (s *ShopServiceImpl) DeleteCanteen(canteenID string) error {
     return nil
+}
+
+func (s *ShopServiceImpl) GetShopByAdminID(adminID uuid.UUID) (*entities.Shop, error) {
+    return s.ShopRepository.GetShopByAdminID(adminID)
+}
+
+func (s *ShopServiceImpl) ChangeState(body model.ChangeState, shopID uuid.UUID) error {
+
+	var state bool
+	switch body.State {
+	case "open":
+		state = true
+	case "close":
+		state = false
+	default:
+		return fmt.Errorf("invalid state value: %s", body.State)
+	}
+
+	return s.ShopRepository.ChangeState(state,shopID)
+}
+
+func (s *ShopServiceImpl) EditShopImage(shopID uuid.UUID, imageModel *utils.ImageModel) error {
+    err := s.ShopRepository.EditShopImage(shopID, imageModel)
+    if err != nil {
+        return err
+    }
+    return nil
+}
+
+func (s *ShopServiceImpl) EditShop(body model.EditShopRequest, shop uuid.UUID) error {
+	if body.ShopName == "" && body.ShopCanteenName == "" {
+		return errors.New("no fields to update")
+	}
+
+
+	shopEntity := &entities.Shop{
+		Name:        body.ShopName,
+		CanteenName: body.ShopCanteenName,
+		Address:     "Default Address",
+	}
+    fmt.Println("Service - EditShop: ", shopEntity, shop)
+
+	return s.ShopRepository.EditShop(*shopEntity, shop)
+}
+
+func (s *ShopServiceImpl) GetShopAdminByUsername(username string) (*entities.ShopAdmin, error) {
+	return s.ShopRepository.GetShopAdminByUsername(username)
 }
