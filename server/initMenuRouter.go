1e05390a16a46e20841b74976f31369522c25099
suwichk007 <suwichakboatkung@gmail.com>
2025-09-14 14:10:00 +0700

    refactor: comment out menu router initialization in server startup

--- server/initMenuRouter.go
+++ server/initMenuRouter.go
@@ -2,16 +2,16 @@
 package server
 
 import (
-	_menuController "hewhew-backend/pkg/menu/controller"
-	_menuRepository "hewhew-backend/pkg/menu/repository"
-	_menuService "hewhew-backend/pkg/menu/service"
+//	_menuController "hewhew-backend/pkg/menu/controller"
+//	_menuRepository "hewhew-backend/pkg/menu/repository"
+//	_menuService "hewhew-backend/pkg/menu/service"
 )
 
-func (s *fiberServer) initMenuRouter() {
-	menuRepository := _menuRepository.NewMenuRepositoryImpl(s.db)
-	menuService := _menuService.NewMenuServiceImpl(menuRepository)
-	menuController := _menuController.NewMenuControllerImpl(menuService)
+//func (s *fiberServer) initMenuRouter() {
+//	menuRepository := _menuRepository.NewMenuRepositoryImpl(s.db)
+//	menuService := _menuService.NewMenuServiceImpl(menuRepository)
+//	menuController := _menuController.NewMenuControllerImpl(menuService)
 
-	menuGroup := s.app.Group("/v1/menu")
-	menuGroup.Post("/", menuController.CreateMenu)
-}
+//	menuGroup := s.app.Group("/v1/menu")
+//	menuGroup.Post("/", menuController.CreateMenu)
+//}
