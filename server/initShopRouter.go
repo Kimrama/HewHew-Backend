57abe9163e0bb2484816a88b8b565321686ac8f8
suwichk007 <suwichakboatkung@gmail.com>
2025-09-22 15:19:29 +0700

    Rename ChangeState endpoint to toggle_open_state for clarity

--- server/initShopRouter.go
+++ server/initShopRouter.go
@@ -22,7 +22,7 @@ func (s *fiberServer) initShopRouter() {
 	shopGroup.Use(utils.JWTProtected())
 	shopGroup.Put("/", shopController.EditShop)
 	shopGroup.Get("/", shopController.GetShop)
-	shopGroup.Put("/patch", shopController.ChangeState)
+	shopGroup.Put("/toggle_open_state", shopController.ChangeState)
 	shopGroup.Put("/shopimage", shopController.EditShopImage)
 
 	shopGroup.Post("/tags", shopController.Createtag)
